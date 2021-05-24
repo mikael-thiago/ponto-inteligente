635e87bbddc95f8b89916311566fbb186d9fb814
Mikael <mikaelbad12@academico.ufs.br>
2021-05-24 15:53:26 -0300

    [00] Criando projeto Spring Boot

---
+++ src/main/java/transformers/labsit/io/pontointeligente/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package transformers.labsit.io.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
